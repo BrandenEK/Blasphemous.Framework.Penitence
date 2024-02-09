31422cf58962f84c39834a91e779cdc253ea3470
unknown <brandenek03912@hotmail.com>
2024-02-09 11:34:52 -0500

    init: add base project files

---
+++ Blasphemous.Framework.Penitence/Main.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace Blasphemous.Framework.Penitence;
+
+[BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
+[BepInDependency("Blasphemous.ModdingAPI", "2.0.2")]
+public class Main : BaseUnityPlugin
+{
+    public static PenitenceFramework PenitenceFramework { get; private set; }
+
+    private void Start()
+    {
+        PenitenceFramework = new PenitenceFramework();
+    }
+}
---
+++ Blasphemous.Framework.Penitence/PenitenceFramework.cs
@@ -0,0 +1,13 @@
+﻿using Blasphemous.ModdingAPI;
+
+namespace Blasphemous.Framework.Penitence;
+
+public class PenitenceFramework : BlasMod
+{
+    public PenitenceFramework() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+    protected override void OnInitialize()
+    {
+        LogError($"{ModInfo.MOD_NAME} has been initialized");
+    }
+}
