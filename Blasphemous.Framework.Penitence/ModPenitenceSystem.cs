b54b8ff26ba72f606b7ae9d5de2d8083802bb138
unknown <brandenek03912@hotmail.com>
2024-02-09 18:16:17 -0500

    core: add api code

---
+++ Blasphemous.Framework.Penitence/ModPenitenceSystem.cs
@@ -0,0 +1,27 @@
+﻿using Framework.Penitences;
+
+namespace Blasphemous.Framework.Penitence;
+
+internal class ModPenitenceSystem : IPenitence
+{
+    public string id;
+    public string Id => id;
+
+    public bool Completed { get; set; }
+    public bool Abandoned { get; set; }
+
+    public void Activate()
+    {
+        Main.PenitenceFramework.ActivatePenitence(Id);
+    }
+
+    public void Deactivate()
+    {
+        Main.PenitenceFramework.DeactivatePenitence(Id);
+    }
+
+    public ModPenitenceSystem(string id)
+    {
+        this.id = id;
+    }
+}
