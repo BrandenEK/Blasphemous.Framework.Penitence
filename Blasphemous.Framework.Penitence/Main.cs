3fbf7bb6f15a31f817264aa61795489ad519bad0
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-02-03 15:57:16 -0500

    core: upgrade api

--- Blasphemous.Framework.Penitence/Main.cs
+++ Blasphemous.Framework.Penitence/Main.cs
@@ -3,7 +3,7 @@
 namespace Blasphemous.Framework.Penitence;
 
 [BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
-[BepInDependency("Blasphemous.ModdingAPI", "2.4.1")]
+[BepInDependency("Blasphemous.ModdingAPI", "3.0.0")]
 internal class Main : BaseUnityPlugin
 {
     public static Main Instance { get; private set; }
